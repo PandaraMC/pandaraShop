22b6fffb8bcf3eae07761b87f0d6f5dcd1d0025f
Fernando <contactalvares@gmail.com>
2025-01-25 20:38:17 +0000

    FAWE dependency update to 2.6.3

--- src/main/java/pandaraShop/manager/Admin/RemoveFile.java
+++ src/main/java/pandaraShop/manager/Admin/RemoveFile.java
@@ -2,7 +2,6 @@ package pandaraShop.manager.Admin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import pandaraShop.Main;
 
@@ -12,27 +11,10 @@ import java.util.UUID;
 public class RemoveFile {
 
     private static File file;
-    private static FileConfiguration editFile;
 
     public static void manualRemove(UUID uuid, String string) {
         Player player = Bukkit.getPlayer(uuid);
-        String name = string;
-        file = new File(Bukkit.getServer().getPluginManager().getPlugin("pandaraShop").getDataFolder(), name + ".yml"); //File to be removed
-        if (file.exists()) {
-            file.delete();
-            Main.getInstance().saveConfig();
-            player.sendMessage(ChatColor.GREEN + "File " + file.getName() + " successfully removed.");
-        }
-        else {
-            player.sendMessage(ChatColor.GOLD + "File does not exist, please try again!");
-        }
-    }
-
-    //Remove an existing file!!!
-    public static void autoRemove(UUID uuid) {
-        Player player = Bukkit.getPlayer(uuid);
-        if (player == null) {return;}
-        file = new File(Bukkit.getServer().getPluginManager().getPlugin("pandaraShop").getDataFolder(), player.getUniqueId() + ".yml"); //File to be removed
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("pandaraShop").getDataFolder(), string + ".yml"); //File to be removed
         if (file.exists()) {
             file.delete();
             Main.getInstance().saveConfig();
