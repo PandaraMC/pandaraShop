34f74761e76f7206ea025c99d87dac433f70f999
Fernando <contactalvares@gmail.com>
2025-01-25 20:46:52 +0000

    FAWE dependency update to 2.6.3

--- src/main/java/pandaraShop/manager/Admin/RemoveFile.java
+++ src/main/java/pandaraShop/manager/Admin/RemoveFile.java
@@ -6,19 +6,24 @@ import org.bukkit.entity.Player;
 import pandaraShop.Main;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.UUID;
 
 public class RemoveFile {
 
-    private static File file;
-
     public static void manualRemove(UUID uuid, String string) {
         Player player = Bukkit.getPlayer(uuid);
-        file = new File(Bukkit.getServer().getPluginManager().getPlugin("pandaraShop").getDataFolder(), string + ".yml"); //File to be removed
+        assert player != null;
+        File file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("pandaraShop")).getDataFolder(), string + ".yml"); //File to be removed
         if (file.exists()) {
             file.delete();
             Main.getInstance().saveConfig();
-            player.sendMessage(ChatColor.GREEN + "File " + file.getName() + " successfully removed.");
+            if (file.delete()) { // Check the result of file.delete()
+                player.sendMessage(ChatColor.GREEN + "Your shop has been successfully deleted");
+                Main.getInstance().saveConfig();
+            } else {
+                player.sendMessage(ChatColor.RED + "Failed to delete the shop file. Please try again!");
+            }
         }
         else {
             player.sendMessage(ChatColor.GOLD + "File does not exist, please try again!");
